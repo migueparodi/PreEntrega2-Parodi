cd483e518c7525501c383c77b9b4c7b8750dfa61
mparodi <miguel45496@gmail.com>
2023-05-17 10:33:04 -0300

    subiendo a github

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-alert("Holasss")
+//Inicio
+
+function calcularPromedio() {
+    let suma = 0;
+
+    for (let i = 1; i <= 5; i++) {
+    let nota = parseFloat(prompt("Ingrese su nota " + i + ":"));
+
+      // Validar si la nota es un número válido
+    while (isNaN(nota)) {
+        nota = parseFloat(prompt("Ingrese una nota válida para la nota " + i + ":"));
+    }
+
+    suma += nota;
+    }
+
+    const promedio = suma / 5;
+    return promedio;
+}
+
+const resultado = calcularPromedio();
+alert("El promedio de las notas ingresadas es: " + resultado.toFixed(2));
+
+
+if (resultado <= 5) {
+    alert ("Tenes que esforzarte mas");
+}
+else if (resultado <= 8) {
+    alert ("Vas por buen camino");
+}
+else if (resultado <= 10) {
+    alert ("Estas dando lo mejor de vos");
+}
+
+
+
