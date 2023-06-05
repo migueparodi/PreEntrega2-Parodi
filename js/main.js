1cb64e8b2f566aa813d7536b2cf5669f339fda0f
mparodi <miguel45496@gmail.com>
2023-06-05 18:28:37 -0300

    avanzamos el proyecto

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-//Inicio
+
+const promedios = [];
 
 function calcularPromedio() {
     let suma = 0;
 
-    for (let i = 1; i <= 5; i++) {
+    for (let i = 1; i <= 3; i++) {
     let nota = parseFloat(prompt("Ingrese su nota " + i + ":"));
 
-      // Validar si la nota es un número válido
+    // Validar si la nota es un número válido
     while (isNaN(nota)) {
         nota = parseFloat(prompt("Ingrese una nota válida para la nota " + i + ":"));
     }
@@ -14,23 +15,73 @@ function calcularPromedio() {
     suma += nota;
     }
 
-    const promedio = suma / 5;
+    const promedio = suma / 3;
     return promedio;
 }
 
-const resultado = calcularPromedio();
-alert("El promedio de las notas ingresadas es: " + resultado.toFixed(2));
+// Para agregar un promedio
+function agregarPromedio() {
+    const resultado = calcularPromedio();
+    promedios.push(resultado.toFixed(2));
+}
 
+// Para corregir promedio
+function corregirPromedio() {
+    const index = prompt("Ingrese el índice del promedio a corregir:");
+    const nuevoPromedio = parseFloat(prompt("Ingrese el nuevo promedio:"));
 
-if (resultado <= 5) {
-    alert ("Tenes que esforzarte mas");
+  if (!isNaN(nuevoPromedio)) {
+    promedios[index] = nuevoPromedio.toFixed(2);
+    alert("El promedio ha sido corregido correctamente.");
+    } else {
+    alert("El promedio ingresado no es válido.");
+    }
 }
-else if (resultado <= 8) {
-    alert ("Vas por buen camino");
+
+// Para mostrar los promedios
+function mostrarPromedios() {
+    if (promedios.length === 0) {
+    alert("No se han ingresado promedios.");
+    } else {
+    alert("Promedios ingresados:\n" + promedios.join("\n"));
+    }
 }
-else if (resultado <= 10) {
-    alert ("Estas dando lo mejor de vos");
+
+// Menú principal
+function menuPrincipal() {
+    let opcion = "";
+
+    while (opcion !== "4") {
+    opcion = prompt(
+        "Seleccione una opción:\n" +
+        "1. Agregar nuevo promedio\n" +
+        "2. Corregir promedio existente\n" +
+        "3. Mostrar todos los promedios\n" +
+        "4. Salir"
+    );
+
+    switch (opcion) {
+      case "1":
+        agregarPromedio();
+        break;
+      case "2":
+        corregirPromedio();
+        break;
+      case "3":
+        mostrarPromedios();
+        break;
+      case "4":
+        alert("Gracias por ingresar su promedio, que tenga un buen dia!");
+        break;
+      default:
+        alert("Opción inválida. Por favor, ingrese una correcta.");
+        break;
+    }
+  }
 }
+
+// Iniciar el programa
+menuPrincipal();
 
 
 
